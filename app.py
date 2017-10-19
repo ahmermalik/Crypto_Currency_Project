94397b642dddac51995da8b5e267b3c26cd20492
Monis Rizvi <monisriz@gmail.com>
2017-10-19 10:31:36 -0500

    MR file structures

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ import tornado.log
 from jinja2 import \
     Environment, PackageLoader, select_autoescape
 
-
-
 ENV = Environment(
     loader=PackageLoader('dashboard', 'templates'),
     autoescape=select_autoescape(['html', 'xml'])
 )
+PORT = int(os.environ.get('PORT', '1337'))
 
 
 class TemplateHandler(tornado.web.RequestHandler):
@@ -23,26 +22,25 @@ class TemplateHandler(tornado.web.RequestHandler):
 
 class MainHandler(TemplateHandler):
     def get(self):
-        self.render_template("base.html", {})
+        self.render_template("index.html", {})
 
 
-class PageHandler(TemplateHandler):
-      def get(self, page):
-        self.set_header(
-          'Cache-Control',
-          'no-store, no-cache, must-revalidate, max-age=0')
-        self.render_template(page + '.html', {})
+# class PageHandler(TemplateHandler):
+#       def get(self, page):
+#         self.set_header(
+#           'Cache-Control',
+#           'no-store, no-cache, must-revalidate, max-age=0')
+#         self.render_template(page + '.html', {})
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/page/(.*)", PageHandler),
+        # (r"/page/(.*)", PageHandler),
         (r"/static/(.*)",
          tornado.web.StaticFileHandler, {'path': 'static'}),
     ], autoreload=True)
 
-PORT = int(os.environ.get('PORT', '1337'))
 if __name__ == "__main__":
     tornado.log.enable_pretty_logging()
 
