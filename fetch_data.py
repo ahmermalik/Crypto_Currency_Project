f27a2dcd3bfcb7af6f56601ae6a3508bbb8f4dd3
Ahmer Malik <ahmer9800@gmail.com>
2017-10-22 13:23:33 -0500

    added fetch_data

--- fetch_data.py
+++ fetch_data.py
@@ -1,4 +1,6 @@
 import requests
+import time
+from models import Currency
 url = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
 
 # https://bittrex.com/api/v1.1/public/getmarketsummaries
@@ -10,8 +12,51 @@ url = "https://bittrex.com/api/v1.1/public/getmarketsummaries"
 while True:
     response = requests.request("GET", url).json()['result']
     for item in response:
-        print(item['MarketName'])
-        # print(item)
+
+        coin_pair = item['MarketName']
+        day_high = item['High']
+        day_low = item['Low']
+        volume = item['Volume']
+        last_price = item['Last']
+        base_volume = item['BaseVolume']
+        bid_price = item['Bid']
+        ask_price = item['Ask']
+        open_buy = item['OpenBuyOrders']
+        open_sell = item['OpenSellOrders']
+        prev_day = item['PrevDay']
+
+        currency = Currency.select().where(Currency.coin_pair == coin_pair)
+
+        if not currency:
+            print(coin_pair + ' has been created')
+            Currency.create(coin_pair=coin_pair,
+                            day_high=day_high,
+                            day_low=day_low,
+                            volume=volume,
+                            last_price=last_price,
+                            base_volume=base_volume,
+                            bid_price=bid_price,
+                            ask_price=ask_price,
+                            open_buy=open_buy,
+                            open_sell=open_sell,
+                            prev_day=prev_day).save()
+        elif currency:
+            print(coin_pair + ' has been updated')
+            Currency.update(day_high=day_high,
+                            day_low=day_low,
+                            volume=volume,
+                            last_price=last_price,
+                            base_volume=base_volume,
+                            bid_price=bid_price,
+                            ask_price=ask_price,
+                            open_buy=open_buy,
+                            open_sell=open_sell,
+                            prev_day=prev_day).where(Currency.coin_pair == coin_pair).execute()
+
+    time.sleep(30)
+
+
+
 
     # print(response.json()['result'])
 
